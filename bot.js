d05cb664b01eef47893e15c9a73c3a576ae4445d
Maarten Courtin <maartencourtin@hotmail.com>
2020-12-01 20:39:44 +0100

    Added an embed help menu

--- bot.js
+++ bot.js
@@ -268,6 +268,42 @@ client.on("message", (msg) => {
   if (content.indexOf("${PREFIX}" === 0)) {
     switch (content.substr(1, content.length)) {
       case "help":
+        msg.channel.send({
+          embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL(),
+            },
+            title: "Help menu",
+            description:
+              "Start FC Emergis Bot commands with a `!` to distinguish them from other messages.",
+            fields: [
+              {
+                name: "avatar_url",
+                value: "Let's me retrieve your avatar url.",
+              },
+              {
+                name: "pickup_line",
+                value:
+                  "For those who don't know how to speak to girls nearby the FC Emergis Stadium.",
+              },
+              {
+                name: "joke",
+                value: "Let me tell you a funny joke. 😂",
+              },
+              {
+                name: "fact",
+                value: "Just a fun fact. 🤓",
+              },
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL(),
+              text: "© FC Emergis",
+            },
+          },
+        });
         break;
       case "avatar_url":
         const avatarUrl = msg.author.avatarURL();
